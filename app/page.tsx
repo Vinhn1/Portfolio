e43a348651bdf181eb3b4194ce8987fc1810c53e
Vinhn1 <vinhnguyen04275@gmail.com>
2025-07-05 00:47:51 +0700

    hiển thị phần đánh giá từ khách hàng (testimonials) và logo các công ty đã từng làm việc (companies)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Clients from "@/components/Clients";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -7,23 +8,29 @@ import { FaHome } from "react-icons/fa";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-     <div className="max-w-7xl w-full">
-      {/* Floating Navigation Bar - Hiển thị menu điều hướng nổi */}
-      <FloatingNav navItems={
-        [
-          {
-            name: 'Home',
-            link: '/',
-            icon: <FaHome/>
-          }
-        ]
-      }/>
-      {/* Hero (Giao diện chính) */}
-        <Hero/>
+      <div className="max-w-7xl w-full">
+        {/* Floating Navigation Bar - Hiển thị menu điều hướng nổi */}
+        <FloatingNav
+          navItems={[
+            {
+              name: "Home",
+              link: "/",
+              icon: <FaHome />,
+            },
+          ]}
+        />
+        {/* Hero (Giao diện chính) */}
+        <Hero />
         {/* Grid - Hiển thị lưới các dự án hoặc nội dung chính */}
-        <Grid/>
-        <RecentProjects/>
-     </div>
+        <Grid />
+        {/* RecentProjects - Hiển thị danh sách các dự án gần đây */}
+        <RecentProjects />
+        {/* Clients -   hiển thị phần đánh giá từ khách hàng (testimonials) và logo các công ty đã từng làm việc (companies) */}
+        <Clients />
+        {/* Experience */}
+        {/* Approach */}
+        {/* Footer */}
+      </div>
     </main>
   );
 }
---
+++ components/Clients.tsx
@@ -0,0 +1,44 @@
+"use client"; // 	Cho phép sử dụng logic client-side như animation, state...
+
+import React from "react";
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+import { companies, testimonials } from "@/data";
+
+const Clients = () => {
+  return (
+    <section id="testimonials" className="py-4">
+      {/* Tiêu đề */}
+      <h1 className="heading">
+        Kind words from
+        <span className="text-purple"> satisfied clients</span>
+      </h1>
+      {/* Testimonials -> Các đánh giá */}
+      <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
+        {/* Hiển thị các đánh giá trượt liên tục từ phải qua trái  */}
+        <InfiniteMovingCards
+          items={testimonials} // Các bài đánh giá
+          direction="right" // trượt liên tục từ phải qua trái
+          speed="slow"
+        />
+      </div>
+
+      {/* Logo các công ty làm việc */}
+      <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
+        {companies.map((company) => (
+          <React.Fragment key={company.id}>
+            <div className="flex md:max-w-60 max-w-32 gap-2">
+              <img
+                src={company.img}
+                alt={company.name}
+                className="md:w-10 w-5"
+              />
+              <img src={company.nameImg} alt={company.name} width={company.id === 4 || company.id === 5 ? 100 : 150} className="md:w-24 w-20" />
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Clients;
